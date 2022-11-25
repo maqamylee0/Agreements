bc4dd3d1fadfae0da1dfbd145839c0050a299970
maqamylee0 <maqamylee0@gmail.com>
2022-11-25 21:06:50 +0300

    added font

--- tenant_bureau/lib/main.dart
+++ tenant_bureau/lib/main.dart
@@ -1,5 +1,8 @@
+
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tenant_bureau/register.dart';
 import 'Utils.dart';
 import 'login.dart';
@@ -12,8 +15,10 @@ Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
    await Firebase.initializeApp();
   runApp(MaterialApp(
-
-    scaffoldMessengerKey: Utils.messengerKey,
+    theme: ThemeData(
+      fontFamily: 'Playfair Display',
+    ),
+        scaffoldMessengerKey: Utils.messengerKey,
     navigatorKey: navigatorKey,
     debugShowCheckedModeBanner: false,
     home: Welcome(),
